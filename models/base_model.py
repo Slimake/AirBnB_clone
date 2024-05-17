9cfb23de5f53cadb2b62c09595daaafaa6360535
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-05-17 10:36:08 +0000

    Link BaseModel to FileStorage
    By importing the variable storage and calling save method of storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,29 @@
 """Module base_model"""
 
 from datetime import datetime
+from models import storage
 import uuid
 
 
 class BaseModel():
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel class"""
-        self.id = str(uuid.uuid4())  # random unique id using uuid4
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        """Informal string representation of BaseModel class"""
-        return "[{:s}] ({:s}) {}" \
-            .format(self.__class__.__name__, self.id, self.__dict__)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at":
+                        value = datetime \
+                            .strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    elif key == "updated_at":
+                        value = datetime. \
+                            strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())  # random unique id using uuid4
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """
@@ -24,6 +32,7 @@ class BaseModel():
         with the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
@@ -35,3 +44,8 @@ class BaseModel():
         dictionary['created_at'] = dictionary['created_at'].isoformat()
         dictionary['updated_at'] = dictionary['updated_at'].isoformat()
         return dictionary
+
+    def __str__(self):
+        """Informal string representation of BaseModel class"""
+        return "[{:s}] ({:s}) {}" \
+            .format(self.__class__.__name__, self.id, self.__dict__)
